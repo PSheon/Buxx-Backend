2bd356b52f94021daceaa379fba991144f4ede67
PSheon <pauljiang61020@gmail.com>
2024-04-01 22:22:17 +0800

    feat: added sentry plugin.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,17 @@
-export default () => ({
+export default ({ env }) => ({
   documentation: {
     enabled: true,
     config: {
       info: {
         version: "0.0.1",
       },
+    },
+  },
+  sentry: {
+    enabled: true,
+    config: {
+      dsn: env("SENTRY_DSN"),
+      sendMetadata: true,
     },
   },
 });

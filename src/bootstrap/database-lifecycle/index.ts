ab9594a9dada8785525e762517235cc97083f081
PSheon <pauljiang61020@gmail.com>
2024-04-29 14:31:36 +0800

    feat: added notification into db lifecycle.

--- src/bootstrap/database-lifecycle/index.ts
+++ src/bootstrap/database-lifecycle/index.ts
@@ -7,4 +7,27 @@ import { errors } from "@strapi/utils";
 /* Errors types */
 const { ApplicationError } = errors;
 
-export const databaseLifecycleBootstrap = (strapi) => {};
+export const databaseLifecycleBootstrap = (strapi) => {
+  strapi.db.lifecycles.subscribe({
+    models: ["plugin::users-permissions.user"],
+
+    async afterCreate(event) {
+      const { result } = event;
+
+      const user = result;
+
+      // ** Create Notification entity
+      try {
+        await strapi
+          .service("api::notification.notification")
+          .createWelcomeNotification({
+            notifier: user,
+          });
+      } catch (error) {
+        throw new ApplicationError(
+          `Create notification failed. [${error.message}]`
+        );
+      }
+    },
+  });
+};

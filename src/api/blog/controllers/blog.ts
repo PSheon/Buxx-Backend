45de7ff4de2b0961112a717eabc1339afe4e6d9b
PSheon <pauljiang61020@gmail.com>
2024-05-10 20:01:40 +0800

    feat: added blog api.

---
+++ src/api/blog/controllers/blog.ts
@@ -0,0 +1,10 @@
+/**
+ * blog controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController(
+  "api::blog.blog",
+  ({ strapi }) => ({})
+);
---
+++ src/api/blog/routes/blog.ts
@@ -0,0 +1,24 @@
+/**
+ * blog router
+ */
+
+import { factories } from "@strapi/strapi";
+
+const defaultRouter = factories.createCoreRouter("api::blog.blog");
+
+const customRouter = (defaultRouter, customRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return defaultRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = customRoutes.concat(defaultRouter.routes);
+      return routes;
+    },
+  };
+};
+
+const customRoutes = [];
+
+export default customRouter(defaultRouter, customRoutes);

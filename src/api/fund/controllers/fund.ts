6891f2fce7defca1f10748dfc55a94b563fabf5e
PSheon <pauljiang61020@gmail.com>
2024-05-21 14:47:41 +0800

    feat: added backend sign api.

--- src/api/fund/controllers/fund.ts
+++ src/api/fund/controllers/fund.ts
@@ -6,12 +6,16 @@ import { factories } from "@strapi/strapi";
 import utils from "@strapi/utils";
 
 import { isObject } from "lodash/fp";
+import EthCrypto from "eth-crypto";
+import Web3 from "web3";
+
+import { validateSignHashBody } from "./validation/fund";
 
 import { parseBody } from "../utils";
 
 import type Koa from "koa";
 
-const { ValidationError } = utils.errors;
+const { ValidationError, NotFoundError } = utils.errors;
 
 export default factories.createCoreController(
   "api::fund.fund",
@@ -91,6 +95,51 @@ export default factories.createCoreController(
       });
 
       return this.transformResponse(sanitizedEntity);
+    },
+
+    async signHash(ctx: Koa.Context) {
+      const { id } = ctx.params;
+      await this.validateQuery(ctx);
+
+      const body = parseBody(ctx);
+
+      if (!isObject(body.data)) {
+        throw new ValidationError('Missing "data" payload in the request body');
+      }
+
+      const sanitizedInputData = (await this.sanitizeInput(body.data, ctx)) as {
+        contractName: string;
+        minterAddress: string;
+        slotId: string;
+        value: string;
+      };
+
+      await validateSignHashBody(sanitizedInputData, ctx);
+
+      const entity = await strapi.service("api::fund.fund").findOne(id);
+
+      if (!entity) {
+        throw new NotFoundError("Fund entity not found");
+      }
+      if (
+        !entity.fundSFTContractRootSignerAddress ||
+        !entity.fundSFTContractRootSignerPrivateKey
+      ) {
+        throw new NotFoundError("Fund settings are not completed");
+      }
+
+      const messageHash = Web3.utils.soliditySha3(
+        { type: "string", value: sanitizedInputData.contractName },
+        { type: "address", value: sanitizedInputData.minterAddress },
+        { type: "uint256", value: sanitizedInputData.slotId },
+        { type: "uint256", value: sanitizedInputData.value }
+      ) as string;
+      const signature = EthCrypto.sign(
+        entity.fundSFTContractRootSignerPrivateKey,
+        messageHash
+      );
+
+      ctx.send({ hash: signature });
     },
   })
 );

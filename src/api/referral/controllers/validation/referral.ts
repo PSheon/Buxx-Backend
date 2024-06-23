dce26c3849c44a3ee587565c1fe217dbf54bb6ce
PSheon <pauljiang61020@gmail.com>
2024-06-23 11:53:19 +0800

    fix: fixed join referral id check issue.

--- src/api/referral/controllers/validation/referral.ts
+++ src/api/referral/controllers/validation/referral.ts
@@ -3,7 +3,7 @@ import { yup, validateYupSchema } from "@strapi/utils";
 const joinReferralSchema = yup.object().shape({
   referralId: yup
     .string()
-    .matches(/^[23456789A-HJ-NP-Z]{6}$/, "Invalid referral code")
+    .matches(/^[23456789A-HJ-NP-Z]{8}$/, "Invalid referral code")
     .required(),
 });
 export const validateJoinReferralBody = validateYupSchema(joinReferralSchema);

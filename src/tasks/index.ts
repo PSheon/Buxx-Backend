adf98e2feb272fa3b86b800e9e96fa58fb25d232
PSheon <pauljiang61020@gmail.com>
2024-08-12 14:53:53 +0800

    feat: updated cron task frequency.

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -5,12 +5,14 @@ export default {
   clearTaskLog: {
     task: clearTaskLogTask,
     options: {
-      rule: "0 */3 * * * *",
+      // ** Every day at 00:00:00
+      rule: "0 0 0 * * *",
     },
   },
   fetchTokenTransactions: {
     task: fetchTokenEventLogTask,
     options: {
+      // ** Every 30 seconds
       rule: "*/30 * * * * *",
     },
   },

d799f97f8d17af788d1bff2b9dcf0a0a71b9bc4b
PSheon <pauljiang61020@gmail.com>
2024-08-13 14:38:05 +0800

    feat: removed clear-task-log task.

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,14 +1,6 @@
-import { clearTaskLogTask } from "./clear-task-log";
 import { fetchTokenEventLogTask } from "./fetch-event-log";
 
 export default {
-  clearTaskLog: {
-    task: clearTaskLogTask,
-    options: {
-      // ** Every day at 00:00:00
-      rule: "0 0 0 * * *",
-    },
-  },
   fetchTokenTransactions: {
     task: fetchTokenEventLogTask,
     options: {
